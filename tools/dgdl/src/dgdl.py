331a99740147fcdf3ac8fe2baf3afeeec1ca5504
Simon Wells <siwells@gmail.com>
2012-11-26 17:24:46 +0000

    Added CLI support using argparse & support for writing out verified game description files

--- tools/dgdl/src/dgdl.py
+++ tools/dgdl/src/dgdl.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import antlr3
 import antlr3.tree
@@ -5,65 +6,84 @@ from dgdlLexer import dgdlLexer
 from dgdlParser import dgdlParser
 
 def pretty_print_tree(tree):
+    fragments = []
     indent = 0
     last = ""
     for child in tree.getChildren():
-        if child.text == "{":
+        if child.text == "<EOF>":
+            pass
+        elif child.text == "{":
             indent += 1
-            sys.stdout.write(child.text)
-            sys.stdout.write("\n")
+            fragments.append(child.text)
+            fragments.append("\n")
         elif child.text == "}":
             indent -= 1
-            sys.stdout.write("\n")
-            sys.stdout.write("    "*indent+child.text)
+            fragments.append("\n")
+            fragments.append("    "*indent+child.text)
         elif child.text == ",":
-            sys.stdout.write(child.text + "\n")
+            fragments.append(child.text + "\n")
         elif child.text == ":":
-            sys.stdout.write(child.text)
+            fragments.append(child.text)
         elif child.text == "then":
-            sys.stdout.write("\n"+"    "*indent+child.text)
+            fragments.append("\n"+"    "*indent+child.text)
         elif child.text == "and":
-            sys.stdout.write(" "+child.text)
+            fragments.append(" "+child.text)
         elif child.text == "else":
-            sys.stdout.write(" "+child.text)
+            fragments.append(" "+child.text)
         else:
             if last == ":":
-                sys.stdout.write(child.text)
+                fragments.append(child.text)
             elif last == "if":
-                sys.stdout.write(" "+child.text)
+                fragments.append(" "+child.text)
             elif last == "then":
-                sys.stdout.write(" "+child.text)
+                fragments.append(" "+child.text)
             elif last == "and":
-                sys.stdout.write(" "+child.text)
+                fragments.append(" "+child.text)
             elif last == "else":
-                sys.stdout.write(" "+child.text)
+                fragments.append(" "+child.text)
             else:
-                sys.stdout.write("    "*indent+child.text)
+                fragments.append("    "*indent+child.text)
         last = child.text
+    return ''.join(fragments)
+
+def pretty_print(tree, outfile):
+    if outfile:
+        with open(outfile, 'w') as f:
+            data = pretty_print_tree(tree)
+            f.write(data)
+    else:
+        print pretty_print_tree(tree)
 
 def main():
-    if len(sys.argv) != 2:
-        print "Usage: dgdl file.dgdl"
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="DGDL command line tool. Use to verify and prettyprint DGDL game descriptions.")
     
-    infile = sys.argv[1]
+    parser.add_argument("infile", help="The input DGDL game description to work with.")
+    parser.add_argument("-o", "--out", help="Write the game description to the output file instead of stdout", nargs=1)    
+    parser.add_argument("-p", "--prettyprint", help="prettyprint infile by adding whitespace to increase comprehension", action="store_true")
     
+    args = parser.parse_args()
+    
+    infile = args.infile
     char_stream = antlr3.ANTLRFileStream(infile)
     lexer = dgdlLexer(char_stream)
     tokens = antlr3.CommonTokenStream(lexer)
     
     parser = dgdlParser(tokens)
     
+    outfile = ""
+    if(args.out):
+        outfile = ''.join(args.out)
+    
     try:  
         t = parser.dgdl().tree
     except RecognitionException:
         traceback.print_stack()
     
-    if parser.getNumberOfSyntaxErrors() is 0:
-        print "Using grammar file:\t", repr(parser.grammarFileName)
-        print "Using input file:\t", repr(parser.getSourceName())
-        print "Generates the following AST:"
-        pretty_print_tree(t)
+    if parser.getNumberOfSyntaxErrors() > 0:
+        print "There were errors whilst parsing the supplied input file \""+infile+"\""
+        sys.exit(0)
+    elif args.prettyprint:
+        pretty_print(t, outfile)
 
 if __name__ == "__main__":
     main()
